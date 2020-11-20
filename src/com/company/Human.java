f785d1218216aa4ca0e7ab86802a97d0108989d3
SLNSL <nik231002@mail.ru>
2020-11-20 19:39:19 +0600

    Model

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -2,44 +2,13 @@ package com.company;
 
 import java.util.Objects;
 
-public class Human extends ASomeone{
+public class Human extends ASomeone {
 
-    private String name;
-    
 
-    public Human(String nameOfHuman){
+    public Human(String nameOfHuman) {
         setName(nameOfHuman);
         System.out.println("Существо " + getName() + " создано");
     }
 
 
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public void setName(String nameOfSomeone) {
-        this.name = nameOfSomeone;
-    }
-
-    @Override
-    public String toString() {
-        return "Human{" +
-                "name='" + name + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Human human = (Human) o;
-        return Objects.equals(name, human.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
-    }
 }
