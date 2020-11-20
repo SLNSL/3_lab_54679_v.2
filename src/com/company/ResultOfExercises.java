b0f4a4382ef61c83dce4b782766f72aed28daf88
SLNSL <nik231002@mail.ru>
2020-11-20 19:44:03 +0600

    Model

--- src/com/company/ResultOfExercises.java
+++ src/com/company/ResultOfExercises.java
@@ -4,7 +4,7 @@ public class ResultOfExercises {
     public static void HealthAndAngry(ADog dog, ASomeone human) {
        Health(dog);
         System.out.print(", но");
-        Angry(dog, human);
+        Angry(human);
         System.out.println();
     }
 
@@ -12,7 +12,7 @@ public class ResultOfExercises {
         System.out.print("Физ-зарядка была полезной для самочувствия собаки: " + dog.getName());
     }
 
-    public static void Angry(ADog dog, ASomeone human){
+    public static void Angry(ASomeone human){
         System.out.print("это разозлило человека типа: " + human.getName()  + ", ведь его обязанности: следить за садом");
     }
 }
